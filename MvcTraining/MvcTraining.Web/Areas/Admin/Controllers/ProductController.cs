f37a2d6712db8664ece062cdbcdce49f040b313a
Ross Pace <rpace@avantia-inc.com>
2013-04-11 12:20:44 -0400

    started areas, adding product example files, create registration controller

---
+++ MvcTraining/MvcTraining.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTraining.Web.Areas.Admin.Models;
+using MvcTraining.Web.Models;
+
+namespace MvcTraining.Web.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private MvcTrainingContext db = new MvcTrainingContext();
+
+        //
+        // GET: /Admin/Product/
+
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        //
+        // GET: /Admin/Product/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /Admin/Product/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Product/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        //
+        // GET: /Admin/Product/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // POST: /Admin/Product/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /Admin/Product/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // POST: /Admin/Product/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcTraining/MvcTraining.Web/Areas/Online/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTraining.Web.Areas.Online.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        //
+        // GET: /Online/Registration/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
