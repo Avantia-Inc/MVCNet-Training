b1a63c3d0cd28e723da636743069981af07bba16
Ross Pace <rpace@avantia-inc.com>
2013-05-10 17:14:05 -0400

    store demo

--- MvcTraining/MvcTraining.Web/Areas/Online/Controllers/StoreController.cs
+++ MvcTraining/MvcTraining.Web/Areas/Online/Controllers/StoreController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcTraining.Web.Models;
 
 namespace MvcTraining.Web.Areas.Online.Controllers
 {
     public class StoreController : Controller
     {
+		private MvcTrainingContext _context;
+
+		public StoreController()
+		{
+			_context = new MvcTrainingContext();
+		}
+
         //
         // GET: /Online/Store/
 
         public ActionResult Index()
         {
-            return View();
+            return View(_context.Products);
         }
 
     }
